1db93d4304d8c526eacbe4e9a16dbe6ba4579c5a
Cristian García <cristiangarher128@gmail.com>
2026-02-01 13:12:40 +0100

    Modificado index.php

--- proyecto-daw/index.php
+++ proyecto-daw/index.php
@@ -1,1 +1,21 @@
-{\rtf1}
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <link rel="icon" href="img/ferreteria.jpg">
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+
+<body>
+<?php
+require_once 'php/header.php';
+require_once 'php/footer.php';
+?>
+</body>
+</html>
