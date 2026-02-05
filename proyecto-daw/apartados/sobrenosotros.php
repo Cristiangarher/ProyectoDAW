a542c899529190b212c37fc4beb4488f1ad62552
Cristian García <cristiangarher128@gmail.com>
2026-02-05 19:45:08 +0100

    Creada carpeta apartados con los apartados del header.

---
+++ proyecto-daw/apartados/sobrenosotros.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sobre nosotros</title>
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+    <body>
+    <?php require_once '../php/header.php';?>
+        <form action="" method="POST" id="login">
+            <h1>Sobre nosotros</h1>
+            <input type="text" id="nombre" name="nombre" placeholder="Nombre" required>
+            <input type="email" id="email" name="email" placeholder="Email" required>
+            <textarea id="mensaje" name="mensaje" placeholder="Mensaje" required cols="30" rows="10"></textarea>
+            <button type="submit">Enviar</button>
+        </form>
+    <?php require_once '../php/footer.php'; ?>
+    </body>
+</html>
+
+
