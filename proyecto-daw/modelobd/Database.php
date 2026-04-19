73ff6adfc23a651dde3776669d1e39f294843812
Cristian García <cristiangarher128@gmail.com>
2026-04-19 20:01:22 +0200

    Modificado el archivo Database.php.

--- proyecto-daw/modelobd/Database.php
+++ proyecto-daw/modelobd/Database.php
@@ -7,10 +7,8 @@ class Database {
     public $conexion;
 
     public function __construct() {
-        // Usamos $this-> para llamar a las variables de arriba
         $this->conexion = mysqli_connect($this->host, $this->username, $this->password, $this->db_name);
 
-        // Añadimos una comprobación por si falla la conexión
         if (!$this->conexion) {
             die("Error de conexión: " . mysqli_connect_error());
         }
